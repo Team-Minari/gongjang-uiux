2a34e3d0dab755870bca760ef5938b24340507ba
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-10-09 15:05:40 +0900

    #4 feature: 여러 페이지에서 사용될 Footer.tsx 구현 코드 리뷰 반영 - 코드 리뷰 반영 및 리팩토링 진행

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,26 +1,21 @@
-import Header from "./Header.tsx";
-
 export default function Footer() {
 	return (
-		<>
-			<Header />
-			<footer className="bg-green-400 text-white">
-				<div className="max-w-7xl mx-auto px-30">
-					<div className="mx-auto w-full max-w-screen-xl px-6 py-10 md:px-8 md:py-16">
-						<div className="flex flex-col items-center gap-y-8 text-center">
-							<div className="flex flex-col gap-y-4 md:flex-row md:gap-x-12">
-								<button type="button">About</button>
-								<button type="button">Contact</button>
-								<button type="button">Privacy Policy</button>
-								<button type="button">Terms of Service</button>
-							</div>
-							<div>
-								<p>@2026 Stage Access. All rights reserved.</p>
-							</div>
+		<footer className="bg-green-400 text-white">
+			<div className="max-w-7xl mx-auto px-30">
+				<div className="mx-auto w-full max-w-screen-xl px-6 py-10 md:px-8 md:py-16">
+					<div className="flex flex-col items-center gap-y-8 text-center">
+						<div className="flex flex-col gap-y-4 md:flex-row md:gap-x-12">
+							<button type="button">About</button>
+							<button type="button">Contact</button>
+							<button type="button">Privacy Policy</button>
+							<button type="button">Terms of Service</button>
+						</div>
+						<div>
+							<p>@2026 Stage Access. All rights reserved.</p>
 						</div>
 					</div>
 				</div>
-			</footer>
-		</>
+			</div>
+		</footer>
 	);
 }
