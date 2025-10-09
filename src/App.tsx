5118c48adbbc67a1fb1c19c28cfcb538921abf6c
Eunhyeon Kang <kaeuhy_0315@naver.com>
2025-10-09 14:57:46 +0900

    #4 feature: 여러 페이지에서 사용될 Footer.tsx 구현 - 정적인 Footer.tsx 구현하였습니다.
    Resolves: #4
    See also: None

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import Header from "./components/layout/Header.tsx";
+import Footer from "./components/layout/Footer.tsx";
 
 function App() {
-	return <Header />;
+	return <Footer />;
 }
 
 export default App;
